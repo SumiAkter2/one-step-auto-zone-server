b34236ee0444632d7b12601c975977e624cd493d
SumiAkter2 <akteralisha36@gmail.com>
2023-02-07 14:22:07 +0600

    added post for order

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hnxcxtq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const servicesCollection = client.db("autoZone").collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = await servicesCollection.find(query).toArray();
+      res.send(cursor);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("One step Auto Zone");
