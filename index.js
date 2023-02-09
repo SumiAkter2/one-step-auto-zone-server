a954d8759af467daead273bc70f64118caaa021b
SumiAkter2 <akteralisha36@gmail.com>
2023-02-09 19:36:17 +0600

    try  and others

--- index.js
+++ index.js
@@ -52,8 +52,13 @@ async function run() {
 
     app.get("/services", async (req, res) => {
       // const query = { price: { $gt: 100 } };
-      const query = { price: { $lt: 120 } };
-      // const query = {};
+      // const query = { price: { $lt: 120 } };
+      // const query = { price: { $in: [20, 120] } };
+      // const query = { price: { $nin: [20, 120] } };
+      // const query = {
+      //   $and: [{ price: { $gt: 150 } }, { price: { $gt: 100 } }],
+      // };
+      const query = {};
       const order = req.query.order === "asc" ? 1 : -1;
       const cursor = await servicesCollection
         .find(query)
